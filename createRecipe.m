4fe0ff5bf9753212d7e62cbfe138d1d6021ae627
José Aniceto <jAniceto@users.noreply.github.com>
2021-09-07 16:08:27 +0100

    Update recipe program

--- createRecipe.m
+++ createRecipe.m
@@ -1,7 +1,6 @@
-function createRecipe()
-
 %% INPUTS
-filename = 'recipe2';
+filename = 'recipe';
+columns = 4;
 tsw = 1;
 Cor = 1.5; 
 P1 = 2; 
@@ -10,21 +9,36 @@ P3 = 1;
 P4 = 1;
 
 
-%% CREATE RECIPE FILE
-valv_pos = {'4;5;6;7;0;1;2;3';
-            '5;6;7;0;1;2;3;4';
-            '6;7;0;1;2;3;4;5';
-            '7;0;1;2;3;4;5;6';
-            '0;1;2;3;4;5;6;7';
-            '1;2;3;4;5;6;7;0';
-            '2;3;4;5;6;7;0;1';
-            '3;4;5;6;7;0;1;2'};
+%% Checks
+if ~ismember(columns, [4 8])
+    error('Number of columns is not supported.')
+end
+
+
+%% Create recipe
+valv_pos_8cols = {'6;7;0;1;2;3;4;5';
+                  '5;6;7;0;1;2;3;4';
+                  '4;5;6;7;0;1;2;3';
+                  '3;4;5;6;7;0;1;2';
+                  '2;3;4;5;6;7;0;1';
+                  '1;2;3;4;5;6;7;0';
+                  '0;1;2;3;4;5;6;7';
+                  '7;0;1;2;3;4;5;6'};
+              
+valv_pos_4cols = {'6;1;0;1;2;1;4;1';
+                  '4;1;6;1;0;1;2;1';
+                  '2;1;4;1;6;1;0;1';
+                  '0;1;2;1;4;1;6;1'};
         
-fileID = fopen(strcat(filename,'.txt'),'a');  % open text file
+fileID = fopen(strcat(filename,'.txt'),'w');  % open text file
 
 % Each line (recipe_line) format is:  tsw; Cor; P1; P2; P3; P4; VA; VB; VC; VD; VE; VF; VG; VH
-for i=1:8
-    recipe_line = sprintf('%.2f;%.2f;%.2f;%.2f;%.2f;%.2f;%s',tsw,Cor,P1,P2,P3,P4,valv_pos{i});  % create a string for the current line (switch)
+for i=1:columns
+    if columns == 4
+        recipe_line = sprintf('%.2f;%.2f;%.2f;%.2f;%.2f;%.2f;%s',tsw,Cor,P1,P2,P3,P4,valv_pos_4cols{i});  % create a string for the current line (switch)
+    elseif columns == 8
+        recipe_line = sprintf('%.2f;%.2f;%.2f;%.2f;%.2f;%.2f;%s',tsw,Cor,P1,P2,P3,P4,valv_pos_8cols{i});  % create a string for the current line (switch)
+    end
     recipe_line = strrep(recipe_line,'.',',');  % replace '.' by ','
     fprintf(fileID,'%s',recipe_line);  % print string to file
     fprintf(fileID,'\r\n');  % print newline character to file
