67c2a1661af2afae5ec45832792b44d81f811519
José Aniceto <jAniceto@users.noreply.github.com>
2024-07-29 12:16:43 +0100

    Update flowRatesSMB.m

--- flowRatesSMB.m
+++ flowRatesSMB.m
@@ -1,21 +1,40 @@
-% Given eluent, feed, extract and zone I flow-rates, calculate all
-% remaining classic SMB flow-rates.
+% Calculate SMB flow-rates
 clc
 
-%% INPUTS:
-Qeluent = 2/1;
-Qfeed = 1/1;
-Qextract = 1.5/1;
-Qzone1 = 3/1;
+%% INPUTS
+P1 = 'QE';
+P2 = 'QX';
+P3 = 'Q3';
+P4 = 'QF';
+Cor = 'Q2';
+
+QE = 2/1;
+QF = 1/1;
+QX = 1.5/1;
+Q1 = 3/1;
 
 %% CALCULATIONS
-Qzone2 = Qzone1 - Qextract;
-Qzone3 = Qzone2 + Qfeed;
-Qzone4 = Qzone1 - Qeluent;
-Qraffinate = Qzone3 - Qzone4;
+if exist('QE', 'var') && exist('QX', 'var') && exist('QF', 'var') && exist('Q1', 'var')
+    Q2 = Q1 - QE;
+    Q3 = Q2 + QF;
+    Q4 = Q1 - QE;
+    QR = Q3 - Q4;
 
-%% PRINTING RESULTS
+else
+    error('Fail')
+end
+
+
+%% PRINT
 fprintf('Flow-rates obtained from mass balance:\n')
-fprintf('Q1 = %.2f  |  Q2 = %.2f  |  Q3 = %.2f  |  Q4 = %.2f\n', Qzone1, Qzone2, Qzone3, Qzone4)
-fprintf('QF = %.2f  |  QR = %.2f  |  QE = %.2f  |  QX = %.2f\n\n', Qfeed, Qraffinate, Qeluent, Qextract)
-fprintf('Flow rates for SMB:\nP1 (eluent): %.2f \nP2 (zone2): %.2f \nP3 (zone4): %.2f \nP4 (feed): %.2f \nCoriolis (extract): %.2f\n', Qeluent, Qzone2, Qzone4, Qfeed, Qextract)
+fprintf('Q1 = %.2f  |  Q2 = %.2f  |  Q3 = %.2f  |  Q4 = %.2f\n', Q1, Q2, Q3, Q4)
+fprintf('QF = %.2f  |  QR = %.2f  |  QE = %.2f  |  QX = %.2f\n\n', QF, QR, QE, QX)
+
+fprintf([ ...
+    'Flow-rates for SMB:\n' ...
+    'P1 (%s): %.2f \n' ...
+    'P2 (%s): %.2f \n' ...
+    'P3 (%s): %.2f \n' ...
+    'P4 (%s): %.2f \n' ...
+    'Coriolis (%s): %.2f\n\n' ...
+], P1, eval(P1), P2, eval(P2), P3, eval(P3), P4, eval(P4), Cor, eval(Cor))
