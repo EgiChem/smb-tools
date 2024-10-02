4fffb1398652e379418506e2541879437641d771
José Aniceto <jAniceto@users.noreply.github.com>
2024-10-02 18:34:12 +0100

    Update calcQ.m

--- calcQ.m
+++ calcQ.m
@@ -1,6 +1,7 @@
 function Q = calcQ(volume, time)
 % Calculate a flow rate given a volume and time string, for instance, for
 % when measuring flow rates with glassware and chronometer.
+%
 % volume in ml
 % time as 'minutes:seconds' e.g., '3:15'
 % Outouted flow rate in ml/min
