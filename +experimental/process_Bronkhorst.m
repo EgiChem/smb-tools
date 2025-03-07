749b94c0fb889d1530ee48a86375caf4e07a2f7e
José Aniceto <jAniceto@users.noreply.github.com>
2025-03-07 22:43:41 +0000

    Make log processing scripts into functions

--- +experimental/process_Bronkhorst.m
+++ +experimental/process_Bronkhorst.m
@@ -1,73 +1,93 @@
-% Load Bronkhorst FlowSuit flow rate data from Coriolis
+function out = process_Bronkhorst(filepath, signals, make_plots)
+% Load Bronkhorst FlowSuit2 flow rate data from Coriolis.
+%
+% Input the CSV log file path (including extension) in the filepath variable.
+% The script creates flow-rate plot.
+%
+% Arguments:
+%   filepath (char) : path to log file
+%   signals (char) : if the script should process all Coriolis data ('all') or only
+%   Corioris 1 ('one') or Coriolis 2 ('two').
+%
+% Returns:
+%   (struct) : Structure containing all collected data: time, flow rate set
+%   point and value for each Coriolis.
+%
+% Example:
+%   experimental.process_Bronkhorst('sample-files/Bronkhorst FlowSuite Export 1 sec 2025_01_17 16_10_20.csv')
+%
+% Author: José Aniceto
 
-%% INPUTS
-datafiles = {
-    'sample-files/Bronkhorst FlowSuite Export 1 sec 2025_01_17 16_10_20.csv'
-};
 
-signals = 'all';  % which signals to plot (all, one or two)
-
-
-%% LOAD AND PROCESS DATA
-n_files = numel(datafiles);
-
-if n_files > 3
-    warning('Too many data files may make plots difficult to read. Try reducing the number of datafiles to analyze at once.')
+arguments
+    filepath (1,:) char
+    signals (1,:) char {mustBeMember(signals, {'all', 'one', 'two'})} = 'all'
+    make_plots = true
 end
-
-% Set default styles for plots
-set(0, 'DefaultLineLineWidth', 1.5);
 
 fprintf('Processing data from log file... \n');
 
 ymax = 1;  % initial y-axis max range
-for i = 1:n_files
 
-    % Load CSV file
-    filepath = datafiles{i};
-    fprintf('%s \n', filepath);
-    data = readtable(filepath, 'Delimiter', ';', 'DecimalSeparator', ',');
-    
-    % Calculate elapsed time
-    data.ElapsedTime = minutes(data.TimeStamp - data.TimeStamp(1));
-    
-    % Plot flow-rates
+% Load CSV file
+fprintf('%s \n', filepath);
+data = readtable(filepath, 'Delimiter', ';', 'DecimalSeparator', ',');
+
+% Number of signals
+n_cols = width(data);
+n_signals = (n_cols - 1) / 2;  % there are 2 columns per signal (setpoint and flow) and a time column
+
+% Calculate elapsed time
+data.time = minutes(data.TimeStamp - data.TimeStamp(1));
+
+out.time = data.time;
+for i = 1:n_signals
+    out.(sprintf('setpoint%i', i)) = data{:,1+i};
+    out.(sprintf('flow%i', i)) = data{:,1+i+1};
+end
+
+% Plot flow-rates
+if make_plots
+    % Set default styles for plots
+    set(0, 'DefaultLineLineWidth', 1.5);
+
     figure(1)
-    subplot(n_files, 1, i)
     legend_array = {};
+    
     if strcmp(signals, 'one') || strcmp(signals, 'all')
-        plot(data.ElapsedTime, data{:,3}, LineStyle='-')  % Coriolis 1 flow
+        plot(data.time, data{:,3}, LineStyle='-')  % Coriolis 1 flow
         hold on;
-        plot(data.ElapsedTime, data{:,2}, LineStyle='--')  % Coriolis 1 set-point
+        plot(data.time, data{:,2}, LineStyle='--')  % Coriolis 1 set-point
         legend_array = [legend_array, {'MFC1 flow', 'MFC1 setpoint'}];
     end
+    
     if strcmp(signals, 'two') || strcmp(signals, 'all')
-        plot(data.ElapsedTime, data{:,5}, LineStyle='-')  % Coriolis 2 flow
+        plot(data.time, data{:,5}, LineStyle='-')  % Coriolis 2 flow
         hold on;
-        plot(data.ElapsedTime, data{:,4}, LineStyle='--')  % Coriolis 2 set-point
+        plot(data.time, data{:,4}, LineStyle='--')  % Coriolis 2 set-point
         legend_array = [legend_array, {'MFC2 flow', 'MFC2 setpoint'}];
     end
-
+    
     % Get y-axis max value and update final ymax if needed
     current_ymax = max( [max(data{:,3}), max(data{:,5})] );
     if current_ymax > ymax
         ymax = current_ymax;
     end
+    
+    % Format plots
+    for i = 1:1
+        subplot(1, 1, i)
+        ylim([0 ymax])
+        ylabel('Flow (ml/min)')
+    end
+    xlabel('Time (min)')
+    legend(legend_array, Location="southeast")
+    
+    % Print info
+    fprintf('Plot ranges:\n');
+    fprintf('x-axis: 0 - %.2f\n', max(data.time));
+    fprintf('y-axis: 0 - %.2f\n', ymax);
+    fprintf('To change plot axis you can run the following command: \n');
+    fprintf('axis([xmin xmax  ymin ymax])\n\n');
 
 end
-
-% Format plots
-for i = 1:n_files
-    subplot(n_files, 1, i)
-    ylim([0 ymax])
-    ylabel('Flow (ml/min)')
-end
-xlabel('Time (min)')
-legend(legend_array, Location="southeast")
-
-% Print info
-fprintf('Plot ranges:\n');
-fprintf('x-axis: 0 - %.2f\n', max(data.ElapsedTime));
-fprintf('y-axis: 0 - %.2f\n', ymax);
-fprintf('To change plot axis you can run the following command: \n');
-fprintf('axis([xmin xmax  ymin ymax])\n\n');
